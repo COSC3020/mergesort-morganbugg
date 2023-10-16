46073e60bb730f6ec5e718f76241257b1760b632
heckogeck0 <mbugg02@gmail.com>
2023-10-16 17:43:24 -0600

    first attempt

--- README.md
+++ README.md
@@ -15,3 +15,13 @@ part of the array each recursive call considers.
 Analyse the time complexity of your implementation and give a $\Theta$ bound for
 its worst-case runtime. Add your answer, including your reasoning, to this
 markdown file.
+
+### Answer
+
+The implementation used here uses two main loops.  The outer one iterates over the subarray size, and the inner loop iterates over the array jumping by double the subarray size.  The inner while loops might seem like they would cause a larger increase, but the actual merging process should only take linear time for the subarrays.  This many loops seems like it should increase time complexity by a great deal, but because the subarray size doubles with each iteration of the outer loop, the time complexity should only be $\Theta(nlogn)$
+
+## Sources:
+
+I used the following source after a few attempts at an approach more similar to the example of merge sort in the slides.  While I was able to get close to a solution, I was running into problems with how I wanted to traverse the arrays used.
+
+https://www.geeksforgeeks.org/iterative-merge-sort/#
--- code.js
+++ code.js
@@ -1,3 +1,40 @@
 function mergesort(array) {
+    var tmpArr = array.slice()
+
+    // i = subarray size
+    for (i = 1; i < array.length; i = i * 2) {
+        for (j = 0; j < array.length; j += i * 2) {
+            var k = j
+            var left = j
+            var right = Math.min(left + i, array.length)
+            var leftEnd = right
+            var rightEnd = Math.min(right + i, array.length)
+
+            while (left < leftEnd && right < rightEnd) {
+                if (array[left] <= array[right]) {
+                    tmpArr[k] = array[left]
+                    k++
+                    left++
+                } else {
+                    tmpArr[k] = array[right]
+                    k++
+                    right++
+                }
+            }
+            while (left < leftEnd) {
+                tmpArr[k] = array[left]
+                k++
+                left++
+            }
+            while (right < rightEnd) {
+                tmpArr[k] = array[right]
+                k++
+                right++
+            }
+        }
+        var tmp = array
+        array = tmpArr
+        tmpArr = tmp
+    }
     return array;
 }
